492059d3c238c5d3c69f291ddde980e79421f561
ZhangWurui <1136899200@qq.com>
2019-06-21 17:52:49 +0800

    修改userlist未定义bug和更改html样式

--- cmdb/views.py
+++ cmdb/views.py
@@ -9,7 +9,7 @@ def index(request):
         pwd = request.POST.get("pwd",None)
         # 添加数据到数据库
         models.UserInfo.objects.create(user=username,pwd=pwd)
-        # 从数据库里读取数据
-        user_list = models.UserInfo.objects.all()
+    # 从数据库里读取数据
+    user_list = models.UserInfo.objects.all()
     return render(request, "index.html",{"data":user_list})
     
