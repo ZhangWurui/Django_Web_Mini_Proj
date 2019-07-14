9a6bfc7632088c3dd62f59e7d3bc0eea3c92b39a
ZhangWurui <github@ZhangWurui.com>
2019-07-14 23:14:17 +0800

    增加：后台管理数据库界面
    完善数据库“文章”表结构

--- cmdb/admin.py
+++ cmdb/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from cmdb.models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'create_time', 'categories', 'title', 'last_edit_time')
+    search_fields = ('title',)
+
+admin.site.register(Article, ArticleAdmin)
--- cmdb/models.py
+++ cmdb/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    id = models.AutoField(primary_key=True)
-    categories = models.CharField(max_length=50)
-    title = models.CharField(max_length=50)
-    time = models.DateTimeField()
-    content = models.TextField()
+    id = models.AutoField(primary_key=True, verbose_name='序号')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    last_edit_time = models.DateTimeField(auto_now=True, verbose_name='最后一次编辑')
+    categories = models.CharField(choices = [
+        ('F', 'FilmReview'),
+        ('T', 'Tech'),
+        ('O','Other')
+    ], max_length=20, verbose_name='类别')
+    title = models.CharField(max_length=50, verbose_name='标题')
+    preview_Img = models.TextField(default='data:image/jpg;base64,', name='预览图片')
+    content = models.TextField(default='<p>null<p>', name='正文')
