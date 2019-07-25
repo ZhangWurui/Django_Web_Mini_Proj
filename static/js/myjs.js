9d7bfb5c8216fc62151f234c4d0aeb7cc65c900a
ZhangWurui <1136899200@qq.com>
2019-07-26 00:49:10 +0800

    增加：对应不同主题颜色的css文件
    补充根据百度api判断用户输入的语句的情感，而改变颜色

---
+++ static/js/myjs.js
@@ -0,0 +1,11 @@
+function setUserTheme() {
+    var color = localStorage.getItem('color')
+    if (color != '') {
+        // 1:optimistic  2:pessimistic 3.neutral
+        if (color == 'red') {
+            document.getElementById("mycss").setAttribute("href", "/static/css/home_red.css")
+        } else if (color == 'blue') {
+            document.getElementById("mycss").setAttribute("href", "/static/css/home_blue.css")
+        }
+    }
+}
