1aff20841553ceb7fd304194b8c90d389080c0bc
ZhangWurui <1136899200@qq.com>
2019-07-29 15:54:04 +0800

    修改：文章数据表默认值规范化

--- cmdb/models.py
+++ cmdb/models.py
@@ -12,5 +12,5 @@ class Article(models.Model):
     ], max_length=20, verbose_name='类别')
     title = models.CharField(max_length=50, verbose_name='标题')
     preview_Img = models.TextField(default='data:image/jpg;base64,', verbose_name='预览图片')
-    preview_des = models.TextField(default='null', verbose_name='简介')
-    content = models.TextField(default='<p>null<p>', verbose_name='正文')
+    preview_des = models.TextField(default='<p class="my_single_des">&nbsp; &nbsp; &nbsp; &nbsp;</p>', verbose_name='简介')
+    content = models.TextField(default='<p>null</p>', verbose_name='正文')
